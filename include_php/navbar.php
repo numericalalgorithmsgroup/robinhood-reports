43b7c16df05eb47b8f4a9c162f816f295d7f2cd4
Shawn Hall <shawnahall71@gmail.com>
2016-06-13 09:04:10 -0500

    Added and linked in big directories page

--- include_php/navbar.php
+++ include_php/navbar.php
@@ -18,7 +18,7 @@
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Special <span class="caret"></span></a>
               <ul class="dropdown-menu">
                 <li><a href="<?php echo (preg_match('/diskstatsdev/', $_SERVER["REQUEST_URI"])) ? "/diskstatsdev" : "/diskstats" ?>/histogram.php">Age Histogram</a></li>
-                <li><a href="#">Big Directories</a></li>
+                <li><a href="<?php echo (preg_match('/diskstatsdev/', $_SERVER["REQUEST_URI"])) ? "/diskstatsdev" : "/diskstats" ?>/bigdir.php">Big Directories</a></li>
                 <li><a href="#">Interesting Files</a></li>
                 <li><a href="#">Print Files</a></li>
                 <li><a href="#">Largest Directories</a></li>
