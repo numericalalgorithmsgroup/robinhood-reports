61f988ffafe1d6513c6b1b2c591f8706135eff46
Shawn Hall <shawnahall71@gmail.com>
2016-06-16 13:12:08 -0500

    Moved the humanize javascript to the end of the document since those aren't needed for a while later.

--- include_php/leading_js.php
+++ include_php/leading_js.php
@@ -1,4 +1,2 @@
     <script src="js/angular.min.js"></script>
     <script src="js/ui-bootstrap-tpls-1.1.2.min.js"></script>
-    <script src="js/humanize.js"></script>
-    <script src="js/angular-humanize.js"></script>
--- include_php/trailing_js.php
+++ include_php/trailing_js.php
@@ -2,6 +2,8 @@
     <script src="js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/humanize.js"></script>
+    <script src="js/angular-humanize.js"></script>
     <script>
     $(document).ready(function() {
         $('a[href="' + this.location.pathname + '"]').parent().addClass('active');
