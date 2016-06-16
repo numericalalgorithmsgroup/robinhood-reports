0424f7a51692e0dea4eb443d75e5e567ead293dc
Shawn Hall <shawnahall71@gmail.com>
2016-06-16 15:00:03 -0500

    Added available space to the summary page.

--- summary_backend.php
+++ summary_backend.php
@@ -29,15 +29,17 @@ foreach ($db_ro_confs as $conf) {
     $oldsizers = $oldsizeresult->fetch_array(MYSQLI_ASSOC);
 
     $percentold = sprintf('%0.2f', ($oldsizers["Oldsize"]/$sizers["Size"])*100);
+    $available = $conf["size"] - $sizers["Size"];
   
     if ($outp != "[") {$outp .= ",";}
     $outp .= '{"File_System":"'       . $conf["fs"] . '",';
     $outp .= '"Number_of_Users":'     . (is_null($userrs["Users"]) ? 0 : $userrs["Users"])  . ',';
     $outp .= '"Total_Space":'         . (is_null($conf["size"]) ? 0 : $conf["size"]) . ',';
-    $outp .= '"Number_of_Files":'     . (is_null($numfilesrs["Files"]) ? 0 : $numfilesrs["Files"])  . ',';
     $outp .= '"Used_Space":'          . (is_null($sizers["Size"]) ? 0 : $sizers["Size"])  . ',';
-    $outp .= '"Number_of_Old_Files":' . (is_null($oldnumfilesrs["Oldfiles"]) ? 0 : $oldnumfilesrs["Oldfiles"])  . ',';
+    $outp .= '"Available_Space":'     . (is_null($available) ? 0 : $available)  . ',';
+    $outp .= '"Number_of_Files":'     . (is_null($numfilesrs["Files"]) ? 0 : $numfilesrs["Files"])  . ',';
     $outp .= '"Size_of_Old_Files":'   . (is_null($oldsizers["Oldsize"]) ? 0 : $oldsizers["Oldsize"])  . ',';
+    $outp .= '"Number_of_Old_Files":' . (is_null($oldnumfilesrs["Oldfiles"]) ? 0 : $oldnumfilesrs["Oldfiles"])  . ',';
     $outp .= '"Percent_Old_Space":'   . (is_null($percentold) ? 0: $percentold) . '}';
     $outp .= "]";
     echo($outp);
